6db614ed9ed19164abbd8710109ba0d2003f088e
Coms <125128463+ComsIndeed@users.noreply.github.com>
2026-01-02 19:25:09 +0800

    Removed other app projects

---
+++ apps/project-2359-rn/app/index.tsx
@@ -0,0 +1,10 @@
+import { YStack, Text, H1 } from 'tamagui'
+
+export default function Index() {
+    return (
+        <YStack f={1} ai="center" jc="center" bg="$background">
+            <H1>Hello World</H1>
+            <Text>Welcome to your project-2359-rn app!</Text>
+        </YStack>
+    )
+}
