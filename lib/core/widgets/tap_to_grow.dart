77347cf78b8a9615bf61ca555613fa6f58d927ca
Coms <125128463+ComsIndeed@users.noreply.github.com>
2026-02-02 19:36:03 +0800

    revert: Restore TapToGrow to original scale animation
    Predictive back with custom origin alignment is not supported by
    Android's system. Keeping TapToGrow's consistent scale-in/out animation.
    Predictive back still works for standard MaterialPageRoute navigations.

--- lib/core/widgets/tap_to_grow.dart
+++ lib/core/widgets/tap_to_grow.dart
@@ -35,88 +35,21 @@ class TapToGrow extends StatelessWidget {
 
       // 3. Push the route with the alignment
       Navigator.of(context).push(
-        _TapToGrowRoute(page: page, alignment: alignment, duration: duration),
+        PageRouteBuilder(
+          transitionDuration: duration,
+          reverseTransitionDuration: duration,
+          pageBuilder: (_, __, ___) => page,
+          transitionsBuilder: (ctx, anim, secAnim, child) {
+            return ScaleTransition(
+              alignment: alignment,
+              scale: CurvedAnimation(parent: anim, curve: Curves.easeOutExpo),
+              child: child,
+            );
+          },
+        ),
       );
     }
 
     return builder(trigger);
-  }
-}
-
-/// Custom route that supports predictive back while keeping the scale-from-origin
-/// animation when pushing. Uses the theme's page transitions for pop animations.
-class _TapToGrowRoute<T> extends PageRoute<T> {
-  final Widget page;
-  final Alignment alignment;
-  final Duration duration;
-
-  _TapToGrowRoute({
-    required this.page,
-    required this.alignment,
-    required this.duration,
-  });
-
-  @override
-  Color? get barrierColor => null;
-
-  @override
-  String? get barrierLabel => null;
-
-  @override
-  bool get maintainState => true;
-
-  @override
-  Duration get transitionDuration => duration;
-
-  @override
-  Duration get reverseTransitionDuration => const Duration(milliseconds: 300);
-
-  @override
-  Widget buildPage(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-  ) {
-    return page;
-  }
-
-  @override
-  Widget buildTransitions(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child,
-  ) {
-    final theme = Theme.of(context).pageTransitionsTheme;
-
-    // Get the platform's transition builder from the theme
-    // This will be PredictiveBackPageTransitionsBuilder on Android
-    final platformTransitionBuilder =
-        theme.builders[Theme.of(context).platform];
-
-    // For the pop animation (when animation is reversing), use the theme's
-    // transition builder to support predictive back
-    if (animation.status == AnimationStatus.reverse) {
-      if (platformTransitionBuilder != null) {
-        return platformTransitionBuilder.buildTransitions(
-          this,
-          context,
-          animation,
-          secondaryAnimation,
-          child,
-        );
-      }
-    }
-
-    // For the push animation, use our custom scale animation
-    return ScaleTransition(
-      alignment: alignment,
-      scale: CurvedAnimation(
-        parent: animation,
-        curve: Curves.easeOutExpo,
-        reverseCurve: Curves.easeInOut,
-      ),
-      child: child,
-    );
   }
 }
