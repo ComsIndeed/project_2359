67161c69a084ef76d0b5f9cf85f5c07548a7331a
Coms <125128463+ComsIndeed@users.noreply.github.com>
2026-02-14 02:27:47 +0800

    [BUILD] Made text extraction view work and happen

--- apps/project_2359/lib/features/source_page/source_page.dart
+++ apps/project_2359/lib/features/source_page/source_page.dart
@@ -1,35 +1,99 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 import 'package:syncfusion_flutter_core/theme.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class SourcePage extends StatelessWidget {
+class SourcePage extends StatefulWidget {
   const SourcePage({super.key, required this.fileBytes});
 
   final Uint8List fileBytes;
 
   @override
+  State<SourcePage> createState() => _SourcePageState();
+}
+
+class _SourcePageState extends State<SourcePage> {
+  final AdvancedDrawerController controller = AdvancedDrawerController();
+  late final PdfDocument document;
+
+  @override
+  void initState() {
+    super.initState();
+    document = PdfDocument(inputBytes: widget.fileBytes);
+  }
+
+  @override
+  void dispose() {
+    document.dispose();
+    super.dispose();
+  }
+
+  String get documentText => PdfTextExtractor(document).extractText();
+  List<TextLine> get documentTextLines =>
+      PdfTextExtractor(document).extractTextLines();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+      body: AdvancedDrawer(
+        animationDuration: Durations.short4,
+        animationCurve: Curves.easeInOutCubic,
+        openScale: 1,
+        openRatio: 1,
+        rtlOpening: true,
+        controller: controller,
+        drawer: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    controller.hideDrawer();
+                  },
+                ),
+                // Text(documentTextLines.map((e) => "${e.text}\n\n").join()),
+                Text(documentText),
+              ],
             ),
-            SizedBox(
-              height: 800,
-              width: double.infinity,
-              child: SfPdfViewerTheme(
-                data: const SfPdfViewerThemeData(),
-                child: SfPdfViewer.memory(fileBytes),
+          ),
+        ),
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  SizedBox(
+                    height: 800,
+                    width: double.infinity,
+                    child: SfPdfViewerTheme(
+                      data: const SfPdfViewerThemeData(),
+                      child: SfPdfViewer.memory(widget.fileBytes),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.centerRight,
+                child: FilledButton(
+                  child: Icon(Icons.arrow_left),
+                  onPressed: () {
+                    controller.showDrawer();
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
