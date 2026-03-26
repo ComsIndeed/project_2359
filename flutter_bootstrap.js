0768ce38cc6ac432b989d16484593331881123c8
ComsIndeed <ComsIndeed@users.noreply.github.com>
2026-03-26 05:00:31 +0000

    deploy: 57190fcb92d4ed8bb486b55aa93c02eb15154a90

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1091570874" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2442291669" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
