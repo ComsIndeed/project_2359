75c4ea6e3ff46e9158e719dcf1a10c5328d7bfb3
Coms <125128463+ComsIndeed@users.noreply.github.com>
2026-05-08 21:58:41 +0800

    Added note type enum and helpers

---
+++ apps/project_2359/lib/core/models/note_type.dart
@@ -0,0 +1,87 @@
+enum NoteType {
+  basic,
+  basicAndReversed,
+  cloze,
+}
+
+extension NoteTypeExtension on NoteType {
+  String get label {
+    switch (this) {
+      case NoteType.basic:
+        return 'Basic';
+      case NoteType.basicAndReversed:
+        return 'Basic (and reversed card)';
+      case NoteType.cloze:
+        return 'Cloze';
+    }
+  }
+
+  /// Field labels for the UI.
+  List<String> get fieldLabels {
+    switch (this) {
+      case NoteType.basic:
+      case NoteType.basicAndReversed:
+        return ['Front', 'Back'];
+      case NoteType.cloze:
+        return ['Text'];
+    }
+  }
+
+  /// Corresponding column names in the `NoteItems` table.
+  List<String> get fieldColumns {
+    switch (this) {
+      case NoteType.basic:
+      case NoteType.basicAndReversed:
+        return ['front', 'back'];
+      case NoteType.cloze:
+        return ['content'];
+    }
+  }
+
+  /// The number of templates defined for this note type.
+  /// 
+  /// - Basic: 1 (Front -> Back)
+  /// - Basic + Reversed: 2 (Front -> Back, Back -> Front)
+  /// - Cloze: 1 logical template that generates dynamic cards based on content deletions.
+  int get templateCount {
+    switch (this) {
+      case NoteType.basic:
+        return 1;
+      case NoteType.basicAndReversed:
+        return 2;
+      case NoteType.cloze:
+        return 1;
+    }
+  }
+
+  /// Returns the template for the front side of the card for a given ordinal.
+  String getFrontTemplate(int ordinal) {
+    switch (this) {
+      case NoteType.basic:
+        return '{{front}}';
+      case NoteType.basicAndReversed:
+        return ordinal == 0 ? '{{front}}' : '{{back}}';
+      case NoteType.cloze:
+        return '{{cloze:content}}';
+    }
+  }
+
+  /// Returns the template for the back side of the card for a given ordinal.
+  String getBackTemplate(int ordinal) {
+    switch (this) {
+      case NoteType.basic:
+        return '{{back}}';
+      case NoteType.basicAndReversed:
+        return ordinal == 0 ? '{{back}}' : '{{front}}';
+      case NoteType.cloze:
+        return '{{cloze:content}}';
+    }
+  }
+
+  /// Helper to get the field index from a column name.
+  int? getFieldIndex(String column) {
+    final columns = fieldColumns;
+    final index = columns.indexOf(column);
+    return index != -1 ? index : null;
+  }
+}
